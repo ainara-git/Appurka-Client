c43caadc8a25ec963bcd5d89fac43efa3bf1339d
rgalipienzo <rgalipienzo@gmail.com>
2016-11-01 21:26:05 +0100

    create a services library

---
+++ Appurka/Appurka.Services/AuthenticateService.cs
@@ -0,0 +1,15 @@
+﻿using Appurka.Services.Interfaces;
+using System.Threading.Tasks;
+using System;
+
+namespace Appurka.Services
+{
+    public class AuthenticateService : IAuthenticateService
+    {
+        public async Task<bool> LoginAsync(string email, string password)
+        {
+            await Task.Run(() => {  });
+            return true;
+        }
+    }
+}
